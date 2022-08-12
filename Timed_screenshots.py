ca9deda97243475fbd099071117159fc26106905
emmovo <mingjkl@live.com>
2022-08-12 14:15:42 +0800

    Change and update screenshot file recognition logic
    1 Change the new screenshot recognition from the original recognition file name to the recognition file creation date;
    2 Add the function of inputting the working path from an external .pt file;

--- Timed_screenshots.py
+++ Timed_screenshots.py
@@ -1,98 +1,103 @@
-'''
-Author: emmovo mingjkl@live.com
-Date: 2022-08-09 01:02:18
-LastEditors: emmovo mingjkl@live.com
-LastEditTime: 2022-08-09 21:11:32
-FilePath: \screenshot\Timed_screenshots.py
-'''
+
 
 import os
+from queue import Empty
 import re
 from shutil import copyfile
 import time
 import datetime
 from pynput.keyboard import Key, Controller
 
-last_data_max_num = 0
-current_data_max_num = 0
+print("hw")
+
+save_path = "E:\\temp\\TrayIcon\\SavePath.pt"
+save_path_ready = False
+
+dir_target = ""
+dir_orgin = ""
+if os.path.exists(save_path) == True:
+    save_file = open(save_path, 'r', encoding = "utf_8")
+    save_file_lines = save_file.readlines()
+    for line in save_file_lines:
+        if line.startswith("SCREENSHOT_PATH:"):
+            dir_target = line.replace("SCREENSHOT_PATH:", "")
+            dir_target = dir_target.strip()
+            dir_target = dir_target + "/"
+            dir_target = dir_target.replace("/", "\\")
+            print(dir_target)
+        if line.startswith("SAVE_PATH:"):
+            dir_orgin = line.replace("SAVE_PATH:", "")
+            dir_orgin = dir_orgin.strip()
+            dir_orgin = dir_orgin + "/"
+            dir_orgin = dir_orgin.replace("/", "\\")
+            print(dir_orgin)
+
+if (dir_target != "") and (dir_orgin !=""):
+    if os.path.exists(dir_target) and os.path.exists(dir_orgin):
+        save_path_ready = True
 
 
-dir_orgin = "C:\\Users\\mingj\\OneDrive\\图片\\屏幕快照\\"  # 截屏文件保存路径
-dir_target = "D:\\Git\\screenshot\\screenshot\\"    # 截屏目标保存路径
+# dir_orgin = "C:\\Users\\emmov\\OneDrive\\图片\\屏幕快照\\"  # 截屏文件保存路径
+# dir_target = "E:\\temp\\TrayIcon\\screenshot\\"    # 截屏目标保存路径
 
-screenshot_time = 1;
-screenshot_file_mtime = 0;
-keyboard = Controller()
+if save_path_ready == True:
 
-# ============================== 通过按键模拟截图 ============================
+    keyboard = Controller()
 
-while screenshot_file_mtime < screenshot_time:      # 对比截图时间和最大序号文件创建时间
+    finish_screenshot = False
 
-    screenshot_time = time.time();
-    
-    # keyboard.press(Key.shift_l)
-    keyboard.press(Key.print_screen)    # 出发截屏按键
-    time.sleep(0.5)
-    keyboard.release(Key.print_screen)
-    # keyboard.release(Key.shift_l)
-    time.sleep(2)  # 等待截屏完成
+    while finish_screenshot == False: 
+        screenshot_time = time.time()
 
-    dt = datetime.datetime.now()  # 获取当前时间
+        keyboard.press(Key.print_screen)    # 出发截屏按键
+        time.sleep(0.5)
+        keyboard.release(Key.print_screen)
+        time.sleep(2)  # 等待截屏完成
+
+        dt = datetime.datetime.now()  # 获取当前时间
+
+        data_str = dt.strftime("%Y-%m-%d")
+        time_str = dt.strftime("%H-%M-%S")
+
+        if os.path.exists(dir_target + data_str) == False:
+            os.makedirs(dir_target + data_str)
+
+        dir_orgin_list = os.listdir(dir_orgin)  # 获取文件夹内所有文件名清单
+
+        finish_screenshot = False
+        newest_file_path = ""
+
+        for item in dir_orgin_list:
+            dir_orgin_out = dir_orgin + item
+            if (os.path.getmtime(dir_orgin_out)) > screenshot_time:
+                finish_screenshot = True
+                newest_file_path = dir_orgin_out
+                
+    print(newest_file_path)
+
+    # # ============================== 截图转存至设定目录 ============================
+
+    png_target = dir_target  + data_str + "\\" + time_str + '.png'   # 生成保存位置路径
+
+    copyfile(newest_file_path, png_target)     # 复制截图文件至目标文件夹
+    os.remove(newest_file_path)    # 删除原截图文件
+
+    # ============================== 清空 5 天以为的截图记录 ============================
+
+    folder_path = dir_target
+    folder_list = []
+    folder_list = os.listdir(folder_path)
+
+    for file in folder_list:
+        file_path = folder_path + "\\" + file
+        # print(os.path.getmtime(file_path), screenshot_time)
+        if os.path.getmtime(file_path) < (screenshot_time - 432000):
+            file_list = os.listdir(file_path)
+            # print(file_list)
+            for file_d in file_list:
+                os.remove(file_path + '\\' + file_d)
+            os.rmdir(file_path)
 
 
-    # 截图源文件日期格式    eg.2022-08-09 (34).png => 2022-08-09
-    orgin_dir_str = dt.strftime("%Y-%m-%d")
-    # 保存文件格式  04-41-08.png
-    target_dir_str = dt.strftime("%H-%M-%S")
-
-
-    if os.path.exists(dir_target + orgin_dir_str) == False:
-        os.makedirs(dir_target + orgin_dir_str)
-
-
-
-    dir_orgin_list = os.listdir(dir_orgin)  # 获取文件夹内所有文件名清单
-    data_str_pat = re.compile(orgin_dir_str)    # 筛选当日文件的正则表达式
-
-
-    # 提出文件名序号的正则表达式    eg.2022-08-09 (34).png => 34
-    value_pat_str = re.compile("\(\d+\)")
-
-    for line in dir_orgin_list:     # 变量文件名列表
-        if(re.search(data_str_pat, line)):       # 筛选出当日的文件 eg.2022-08-09 (34).png => 2022-08-09
-            current = int(str(value_pat_str.findall(line)).replace(
-                "['(", '').replace(")']", ''))  # 获取当前遍历到的文件序号 eg.2022-08-09 (34).png => 34
-            if current > current_data_max_num:      # 捕获最大的序号
-                current_data_max_num = current      # 保存最大序号
-                max_date_file_name = line            # 保存最大序号的文件文件名
-    
-    dir_orgin_out = dir_orgin + max_date_file_name          # 生成目标路径
-    screenshot_file_mtime = os.path.getmtime(dir_orgin_out)     # 获取最大序号文件的创建时间
-
-    # print("st:", screenshot_time, "fmt:", screenshot_file_mtime)
-
-
-# ============================== 截图转存至设定目录 ============================
-
-dir_target = dir_target + orgin_dir_str + '\\' + target_dir_str + '.png'   # 生成保存位置路径
-
-copyfile(dir_orgin_out, dir_target)     # 复制截图文件至目标文件夹
-os.remove(dir_orgin_out)    # 删除原截图文件
-
-# ============================== 清空 5 天以为的截图记录 ============================
-
-folder_path = ".\\screenshot"
-folder_list = []
-folder_list = os.listdir(folder_path)
-
-for file in folder_list:
-    file_path = folder_path + "\\" + file
-    # print(os.path.getmtime(file_path), screenshot_time)
-    if os.path.getmtime(file_path) < (screenshot_time - 432000):
-        file_list = os.listdir(file_path)
-        # print(file_list)
-        for file_d in file_list:
-            os.remove(file_path + '\\' + file_d)
-        os.rmdir(file_path)
 
 
